22046b9401fd49a553eebb4cfbdb8991c45e0cd7
cjkcoir <j.chellakarthikeyan@gmail.com>
2025-01-22 11:23:09 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Learning From codevolution"); // Logs the string "Learning From codevolution" to the console
+
+const CAKE_ORDERED = "CAKE_ORDERED"; // Defines a constant variable CAKE_ORDERED with the value "CAKE_ORDERED"
+
+function orderCake() {
+  // Declares a function named orderCake
+  return {
+    type: CAKE_ORDERED, // The return statement returns an object with a property type set to the value of CAKE_ORDERED
+    quantity: 1, // The object also includes a property quantity set to 1
+  };
+}
